4b4c38dcb9c6200cd9daa2562ceb60ea13db95d5
Shazan Rizvi <shazanrizvi80@gmail.com>
2024-11-13 02:14:25 +0530

    redis and session config in signup and org

---
+++ Frontend/src/http/axios.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { apiBaseUrl } from '../utils/constants';
+import toast from "react-hot-toast";
+
+const callAPI = async (method, url, data, headers={}) => {
+  try {
+    const options = {
+      method,
+      url: `${apiBaseUrl}${url}`,
+      headers: {
+        ...headers,
+      },
+    };
+
+    if (method === 'POST' || method === 'PUT' || method === 'PATCH') {
+      options.data = data;
+    }
+   
+
+    const response = await axios(options);
+    //console.log("response data from axios call",response.data)
+    if(response?.data?.message){
+      toast.success(response?.data?.message)
+    }
+    
+    return response?.data;
+    
+    
+  } catch (error) {
+     toast.error(error.message);
+    throw error;
+    
+  }
+};
